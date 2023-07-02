1494c10233d0c781c614b6319325d1681471f4a7
KUSHAGRA TOMAR <kushagratomar2016@gmail.com>
2023-07-02 10:53:52 +0530

    commit 2.0

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(
   cors({
     credentials: true,
-    origin: "https://famous-liger-c9efa2.netlify.app/", // client url
+    origin: ["http://localhost:5173", "https://famous-liger-c9efa2.netlify.app"] // client URLs
   })
 );
 
